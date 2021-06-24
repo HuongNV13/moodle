c832e117d82fb2e618034625ef49266da20cdfed
hiendinh <hdpv2@open.ac.uk>
2021-06-24 09:51:41 +0700

    MDL-71938 tool_usertours: New "Display step numbers" setting
    Co-authored-by: Huong Nguyen <huongnv13@gmail.com>

--- admin/tool/usertours/db/upgrade.php
+++ admin/tool/usertours/db/upgrade.php
@@ -99,5 +99,18 @@ function xmldb_tool_usertours_upgrade($oldversion) {
         upgrade_plugin_savepoint(true, 2021092300, 'tool', 'usertours');
     }
 
+    if ($oldversion < 2021100100) {
+        // Define field displaystepnumbers to be added to tool_usertours_tours.
+        $table = new xmldb_table('tool_usertours_tours');
+        $field = new xmldb_field('displaystepnumbers', XMLDB_TYPE_INTEGER, '1', null, true, null, '0', 'configdata');
+
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // User tours savepoint reached.
+        upgrade_plugin_savepoint(true, 2021100100, 'tool', 'usertours');
+    }
+
     return true;
 }
