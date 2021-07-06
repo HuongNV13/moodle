f0afcedd0376eb311faae7f54ca6df40fa903fd3
Simey Lameze <simey@moodle.com>
2021-07-07 07:02:27 +0800

    MDL-71817 block_calendar: fix css styling

--- blocks/calendar_month/block_calendar_month.php
+++ blocks/calendar_month/block_calendar_month.php
@@ -36,7 +36,7 @@ class block_calendar_month extends block_base {
      * @return stdClass the content
      */
     public function get_content() {
-        global $CFG;
+        global $CFG, $PAGE;
 
         require_once($CFG->dirroot.'/calendar/lib.php');
 
@@ -47,6 +47,9 @@ class block_calendar_month extends block_base {
         $this->content = new stdClass;
         $this->content->text = '';
         $this->content->footer = '';
+
+        $renderer = $PAGE->get_renderer('core_calendar');
+        $this->content->text = $renderer->start_layout();
 
         $courseid = $this->page->course->id;
         $categoryid = ($this->page->context->contextlevel === CONTEXT_COURSECAT && !empty($this->page->category)) ?
@@ -60,6 +63,8 @@ class block_calendar_month extends block_base {
         if ($this->page->course->id != SITEID) {
             $this->content->text .= $renderer->event_filter();
         }
+
+        $this->content->text .= $renderer->complete_layout();
 
         return $this->content;
     }
