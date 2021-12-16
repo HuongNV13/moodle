a80d6e19e9513c95a9742a98ca073fdb2e5cafdf
Huong Nguyen <huongnv13@gmail.com>
2021-12-16 15:41:52 +0700

    MDL-72783 usertours: Fix the tour display for pages with drawers

--- admin/tool/usertours/amd/src/tour.js
+++ admin/tool/usertours/amd/src/tour.js
@@ -1435,10 +1435,14 @@ const Tour = class {
 
                 let drawertop = 0;
                 if (targetNode.parents('[data-usertour="scroller"]').length) {
-                    drawertop = targetNode.parents('[data-usertour="scroller"]').scrollTop();
-                    background.css({
-                       position: 'fixed'
-                    });
+                    const scrollerEle = targetNode.parents('[data-usertour="scroller"]');
+                    const navigationBuffer = scrollerEle.offset().top;
+                    if (scrollerEle.scrollTop() >= navigationBuffer) {
+                        drawertop = scrollerEle.scrollTop() - navigationBuffer;
+                        background.css({
+                            position: 'fixed'
+                        });
+                    }
                 }
 
                 background.css({
