3d9dc932cf9c1b897defab7183adb67769f534fa
Huong Nguyen <huongnv13@gmail.com>
2022-09-13 11:27:34 +0700

    MDL-75269 tiny_equation: Create TinyMCE Equation editor plugin

---
+++ lib/editor/tiny/plugins/equation/db/services.php
@@ -0,0 +1,9 @@
+<?php
+$functions = [
+    'tiny_equation_filter' => [
+        'classname' => 'tiny_equation\external\filter',
+        'description' => 'Filter the equation',
+        'type' => 'read',
+        'ajax' => true
+    ],
+];
