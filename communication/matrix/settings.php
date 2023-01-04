b2c3246eca297bd65d2218fd3bf306ad6a2a1c60
Huong Nguyen <huongnv13@gmail.com>
2023-01-04 09:14:29 +0700

    MDL-76699 comm_matrix: Create Moodle Matrix Boilerplate

---
+++ communication/matrix/settings.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Adds admin settings for the plugin.
+ *
+ * @package    comm_matrix
+ * @copyright  2022 Huong Nguyen <huongnv13@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+if ($hassiteconfig) {
+    // TODO: Replace with the real settings.
+    $settings->add(new admin_setting_heading('comm_matrix/pluginname',
+        'Congratulation',
+        'This is a test settings page for the new Moodle Matrix plugin.'
+    ));
+}
