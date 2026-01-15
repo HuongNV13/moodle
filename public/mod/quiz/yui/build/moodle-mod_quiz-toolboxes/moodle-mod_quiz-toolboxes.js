8858059c1b9e7f9d942f94c09b45d19c9706be40
Huong Nguyen <huongnv13@gmail.com>
2026-01-15 11:39:29 +0700

    Test fix quiz delete

--- public/mod/quiz/yui/build/moodle-mod_quiz-toolboxes/moodle-mod_quiz-toolboxes.js
+++ public/mod/quiz/yui/build/moodle-mod_quiz-toolboxes/moodle-mod_quiz-toolboxes.js
@@ -415,7 +415,8 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
         );
 
         // Create the confirmation dialogue.
-        require(['core/notification'], function(Notification) {
+        require(['core/notification', 'core/pending'], function(Notification, Pending) {
+            var pendingPromise = new Pending('mod_quiz/delete:with_confirmation');
             Notification.saveCancelPromise(
                 M.util.get_string('confirm', 'moodle'),
                 M.util.get_string('confirmremovequestion', 'quiz', qtypename),
@@ -436,11 +437,13 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
                             M.core.actionmenu.instance.hideMenu(ev);
                         }
                     }
+                    pendingPromise.resolve();
                 });
 
                 return;
             }.bind(this)).catch(function() {
                 // User cancelled.
+                pendingPromise.resolve();
             });
         }.bind(this));
     },
@@ -520,7 +523,8 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
             return;
         }
 
-        require(['core/notification'], function(Notification) {
+        require(['core/notification', 'core/pending'], function(Notification, Pending) {
+            var pendingPromise = new Pending('mod_quiz/delete_multiple:with_confirmation');
             Notification.saveCancelPromise(
                 M.util.get_string('confirm', 'moodle'),
                 M.util.get_string('areyousureremoveselected', 'quiz'),
@@ -546,11 +550,13 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
                         // Remove the select multiple options.
                         Y.one('body').removeClass(CSS.SELECTMULTIPLE);
                     }
+                    pendingPromise.resolve();
                 });
 
                 return;
             }.bind(this)).catch(function() {
                 // User cancelled.
+                pendingPromise.resolve();
             });
         }.bind(this));
     },
--- public/mod/quiz/yui/src/toolboxes/js/resource.js
+++ public/mod/quiz/yui/src/toolboxes/js/resource.js
@@ -182,7 +182,8 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
         );
 
         // Create the confirmation dialogue.
-        require(['core/notification'], function(Notification) {
+        require(['core/notification', 'core/pending'], function(Notification, Pending) {
+            var pendingPromise = new Pending('mod_quiz/delete:with_confirmation');
             Notification.saveCancelPromise(
                 M.util.get_string('confirm', 'moodle'),
                 M.util.get_string('confirmremovequestion', 'quiz', qtypename),
@@ -203,11 +204,13 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
                             M.core.actionmenu.instance.hideMenu(ev);
                         }
                     }
+                    pendingPromise.resolve();
                 });
 
                 return;
             }.bind(this)).catch(function() {
                 // User cancelled.
+                pendingPromise.resolve();
             });
         }.bind(this));
     },
@@ -287,7 +290,8 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
             return;
         }
 
-        require(['core/notification'], function(Notification) {
+        require(['core/notification', 'core/pending'], function(Notification, Pending) {
+            var pendingPromise = new Pending('mod_quiz/delete_multiple:with_confirmation');
             Notification.saveCancelPromise(
                 M.util.get_string('confirm', 'moodle'),
                 M.util.get_string('areyousureremoveselected', 'quiz'),
@@ -313,11 +317,13 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
                         // Remove the select multiple options.
                         Y.one('body').removeClass(CSS.SELECTMULTIPLE);
                     }
+                    pendingPromise.resolve();
                 });
 
                 return;
             }.bind(this)).catch(function() {
                 // User cancelled.
+                pendingPromise.resolve();
             });
         }.bind(this));
     },
