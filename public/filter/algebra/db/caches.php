f5c948874929775c6e30f0e7039cd8498250ff3c
yusufwib01 <yusuf.wibisono@moodle.com>
2026-04-25 18:54:37 +0700

    MDL-87554 filter: replace dataroot image cache with file storage

--- public/filter/algebra/db/caches.php
+++ public/filter/algebra/db/caches.php
@@ -15,17 +15,21 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Version details
+ * Cache definitions for filter_algebra.
  *
- * @package    filter
- * @subpackage algebra
- * @copyright  1999 onwards Martin Dougiamas (http://dougiamas.com)
+ * @package    filter_algebra
+ * @copyright  2026 Yusuf Wibisono <yusuf.wibisono@moodle.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026042400;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2026041000;        // Requires this Moodle version.
-$plugin->component = 'filter_algebra';  // Full name of the plugin (used for diagnostics)
-$plugin->dependencies = ['filter_tex' => 2026041000];
+$definitions = [
+    'rendered_images' => [
+        'mode' => cache_store::MODE_APPLICATION,
+        'simplekeys' => true,
+        'simpledata' => true,
+        'staticacceleration' => true,
+        'staticaccelerationsize' => 100,
+    ],
+];
